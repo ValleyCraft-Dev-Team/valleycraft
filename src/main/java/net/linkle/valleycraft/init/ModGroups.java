5563298c2aa51c3e8e48b167180fcb4e257210fb
AndEditor7 <35620587+AndEditor7@users.noreply.github.com>
2022-09-19 13:27:04 +1000

    fix group

--- src/main/java/net/linkle/valleycraft/init/ModGroups.java
+++ src/main/java/net/linkle/valleycraft/init/ModGroups.java
@@ -9,8 +9,8 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
 public class ModGroups {
-    public static final Item TIERED_DISHES_ICON = new Item(itemSettings());
-    public static final Item INGREDIENTS_ICON = new Item(itemSettings());
+    public static final Item TIERED_DISHES_ICON = Reg.register("tiered_dishes_icon", new Item(itemSettings()));
+    public static final Item INGREDIENTS_ICON = Reg.register("ingredients_icon", new Item(itemSettings()));
 
     public static final ItemGroup FLORA_GROUP = create("flora", ModBlocks.LAVENDER);
     public static final ItemGroup INGREDIENTS = create("ingredients", ModGroups.INGREDIENTS_ICON);
@@ -18,8 +18,6 @@ public class ModGroups {
     public static final ItemGroup NON_NATURAL_BLOCKS = create("blocks", ModBlocks.APPLE_DOOR);
 
     public static void initialize() {
-        Reg.register("tiered_dishes_icon", TIERED_DISHES_ICON);
-        Reg.register("ingredients_icon", INGREDIENTS_ICON);
     }
     
     private static ItemGroup create(String id, ItemConvertible item) {
