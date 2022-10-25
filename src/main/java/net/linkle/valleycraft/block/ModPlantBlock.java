f70bc3fca0c380c14a9137555cb250cf1fbf4b49
saltyseadoggo <ncamidge@protonmail.com>
2022-10-24 18:52:19 -0700

    More ModPlantBlock cleanup

--- src/main/java/net/linkle/valleycraft/block/ModPlantBlock.java
+++ src/main/java/net/linkle/valleycraft/block/ModPlantBlock.java
@@ -8,17 +8,22 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
+/**
+ * Extension of the vanilla PlantBlock class with a few QOL utilities:
+ * - It uses the vanilla poppy's block settings by default, saving us from extra effort and clutter in NaturalBlocks.
+ * - It can use different VoxelShapes (hitbox shapes) depending on what we give it for each block.
+ */
+
 public class ModPlantBlock extends PlantBlock {
 
     protected VoxelShape shape;
-    protected OffsetType offset = OffsetType.NONE;
 
     /** The ground whitelist for placing plant. */
     protected Predicate<BlockState> groundList = BlockPres.DIRT;
 
     public ModPlantBlock(VoxelShape shape) {
         this(Settings.copy(Blocks.POPPY).offsetType(OffsetType.NONE));
-        shape(shape);
+        this.shape = shape;
     }
     
     public ModPlantBlock(Settings settings) {
@@ -28,12 +33,6 @@ public class ModPlantBlock extends PlantBlock {
     /** Set ground whitelist for placing plant. */
     public ModPlantBlock ground(Predicate<BlockState> predicate) {
         groundList = predicate;
-        return this;
-    }
-    
-    /** Set the VoxelShape (hitbox shape shown when looking at the block) for this plant block. */
-    public ModPlantBlock shape(VoxelShape shape) {
-        this.shape = shape;
         return this;
     }
 
