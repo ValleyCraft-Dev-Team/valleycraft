f02414adf0934e1daf966277219d542db3dfac9f
novaventurelane <linklemodding@gmail.com>
2022-12-13 03:04:54 -0800

    hunter pt 2

--- src/main/java/net/linkle/valleycraft/villager/ModVillagers.java
+++ src/main/java/net/linkle/valleycraft/villager/ModVillagers.java
@@ -44,6 +44,9 @@ public class ModVillagers {
     public static final PointOfInterestType BEEKEEPER_POI = registerPOI("beekeeper_poi", ModBlocks.BEEKEEPER_STATION.block);
     public static final VillagerProfession BEEKEEPER = registerPro("beekeeper",RegistryKey.of(Registry.POINT_OF_INTEREST_TYPE_KEY, new Identifier(Main.ID, "beekeeper_poi")));
 
+    public static final PointOfInterestType HUNTING_POI = registerPOI("hunter_poi", ModBlocks.HUNTING_STATION.block);
+    public static final VillagerProfession HUNTER = registerPro("hunter", RegistryKey.of(Registry.POINT_OF_INTEREST_TYPE_KEY, new Identifier(Main.ID, "hunter_poi")));
+
     //can generate in mining camps
     public static final PointOfInterestType MINER_POI = registerPOI("miner_poi", ModBlocks.MINER_STATION.block);
     public static final VillagerProfession MINER = registerPro("miner", RegistryKey.of(Registry.POINT_OF_INTEREST_TYPE_KEY, new Identifier(Main.ID, "miner_poi")));
