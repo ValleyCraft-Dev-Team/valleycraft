af4d349acddb82f0f6b45811291ca71f4bd5c2b3
AndEditor7 <35620587+AndEditor7@users.noreply.github.com>
2022-12-24 21:12:08 +1100

    Nourishment

---
+++ src/main/java/net/linkle/valleycraft/interfaces/Nourishment.java
@@ -0,0 +1,10 @@
+package net.linkle.valleycraft.interfaces;
+
+/** */
+public interface Nourishment {
+    float SUPERNATURAL = 2.4f;
+    float GOOD = 1.6f;
+    float NORMAL = 1.2f;
+    float LOW = 0.6f;
+    float POOR = 0.2f;
+}
