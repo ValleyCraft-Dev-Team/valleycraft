cfc0e68e5d7846aef4441af28fc3a3a86bb3a890
AndEditor7 <35620587+AndEditor7@users.noreply.github.com>
2022-12-28 19:20:02 +1100

    remove the stronger tnt

--- src/main/java/net/linkle/valleycraft/init/ModEntityType.java
+++ src/main/java/net/linkle/valleycraft/init/ModEntityType.java
@@ -7,12 +7,10 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.linkle.valleycraft.Main;
-import net.linkle.valleycraft.block.entity.StrongerTnTEntity;
 import net.linkle.valleycraft.client.entity.model.pupkins.PupkinEntityModel;
 import net.linkle.valleycraft.client.entity.model.snails.CaveSnailEntityModel;
 import net.linkle.valleycraft.client.entity.model.snails.SculkSnailEntityModel;
 import net.linkle.valleycraft.client.entity.model.snails.SnailEntityModel;
-import net.linkle.valleycraft.client.entity.renderer.blocks.StrongerTnTEntityRenderer;
 import net.linkle.valleycraft.client.entity.renderer.fish.CodEntityRenderer;
 import net.linkle.valleycraft.client.entity.renderer.fish.SalmonEntityRenderer;
 import net.linkle.valleycraft.client.entity.renderer.pupkins.PupkinEntityRenderer;
@@ -21,11 +19,11 @@ import net.linkle.valleycraft.client.entity.renderer.snails.SculkSnailEntityRend
 import net.linkle.valleycraft.client.entity.renderer.snails.SnailEntityRenderer;
 import net.linkle.valleycraft.client.entity.renderer.unused.SoulPetEntityRenderer;
 import net.linkle.valleycraft.entity.fish.*;
+import net.linkle.valleycraft.entity.projectiles.thrown_items.ThrownRockEntity;
 import net.linkle.valleycraft.entity.pupkins.PupkinEntity;
 import net.linkle.valleycraft.entity.snails.CaveSnailEntity;
 import net.linkle.valleycraft.entity.snails.SculkSnailEntity;
 import net.linkle.valleycraft.entity.snails.SnailEntity;
-import net.linkle.valleycraft.entity.projectiles.thrown_items.ThrownRockEntity;
 import net.linkle.valleycraft.entity.unused.SoulPetEntity;
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import net.minecraft.entity.*;
@@ -107,12 +105,7 @@ public class ModEntityType {
                     .trackRangeBlocks(4).trackedUpdateRate(10)
                     .build()
     );
-
-    //blocks
-    public static final EntityType<StrongerTnTEntity> STRONGER_TNT = register("stronger_tnt",
-            FabricEntityTypeBuilder.createMob().entityFactory(StrongerTnTEntity::new).spawnGroup(SpawnGroup.MISC).makeFireImmune()
-                    .trackRangeChunks(10).trackedUpdateRate(10).trackingTickInterval(10).dimensions(EntityDimensions.fixed(0.98F, 0.98F)).build());
-
+    
     public static void initialize() {
         FabricDefaultAttributeRegistry.register(SNAIL, SnailEntity.createSnailAttributes());
         FabricDefaultAttributeRegistry.register(CAVE_SNAIL, CaveSnailEntity.createSnailAttributes());
@@ -151,7 +144,6 @@ public class ModEntityType {
         
         EntityRendererRegistry.register(SOUL_PET, SoulPetEntityRenderer::new);
         EntityRendererRegistry.register(THROWN_ROCK, FlyingItemEntityRenderer::new);
-        EntityRendererRegistry.register(STRONGER_TNT, StrongerTnTEntityRenderer::new);
     }
     
     
