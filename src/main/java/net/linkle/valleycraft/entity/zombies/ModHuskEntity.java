2bfb7e2dfcbd0c8688a8bc460e98ada3bff24f3d
novaventurelane <linklemodding@gmail.com>
2023-04-25 16:22:17 -0700

    small fix

--- src/main/java/net/linkle/valleycraft/entity/zombies/ModHuskEntity.java
+++ src/main/java/net/linkle/valleycraft/entity/zombies/ModHuskEntity.java
@@ -4,8 +4,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.HuskEntity;
 import net.minecraft.world.World;
 
-public class HuskEntity extends net.minecraft.entity.mob.HuskEntity {
-    public HuskEntity(EntityType<? extends net.minecraft.entity.mob.HuskEntity> entityType, World world) {
+public class ModHuskEntity extends HuskEntity {
+    public ModHuskEntity(EntityType<? extends HuskEntity> entityType, World world) {
         super(entityType, world);
     }
 }
--- src/main/java/net/linkle/valleycraft/entity/zombies/ModZombieEntity.java
+++ src/main/java/net/linkle/valleycraft/entity/zombies/ModZombieEntity.java
@@ -4,8 +4,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.world.World;
 
-public class ZombieEntity extends net.minecraft.entity.mob.ZombieEntity {
-    public ZombieEntity(EntityType<? extends net.minecraft.entity.mob.ZombieEntity> entityType, World world) {
+public class ModZombieEntity extends ZombieEntity {
+    public ModZombieEntity(EntityType<? extends ZombieEntity> entityType, World world) {
         super(entityType, world);
     }
 }
